1a94aae09dfb0d0d14c8f253b559f5334d7c4f65
Ylva Andersson <ap2230@mau.se>
2024-03-18 11:11:05 +0100

    Add project files.

---
+++ Database/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";
+
+await using var conn = new NpgsqlConnection(connString);
+await conn.OpenAsync();
+
+// Insert some data
+await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+{
+    cmd.Parameters.AddWithValue("p", "Hello world");
+    await cmd.ExecuteNonQueryAsync();
+}
+
+// Retrieve all rows
+await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+        Console.WriteLine(reader.GetString(0));
+}
